2db87db7bf1514099e8efdb4043c2d05ac5bc8ca
Mohamed <mmuseaden@gmail.com>
2021-10-07 20:23:18 +0100

    take17

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const { DB, NODE_ENV } = require('../config');
 const logger = require('../logger');
-require('dotenv').config();
+// require('dotenv').config();
 
 const devConfig = {
   host: DB.PGHOST,
@@ -16,12 +16,12 @@ const proConfig = {
 }
 
 logger.info('started server in the index.js file off db');
-logger.info(process.env.NODE_ENV);
-logger.info(process.env.NODE_ENV);
-logger.info(process.env.NODE_ENV);
+logger.info(NODE_ENV);
+logger.info(NODE_ENV);
+logger.info(NODE_ENV);
 logger.info('started server in the index.js file off db');
 
-const pool = new Pool(process.env.NODE_ENV ? proConfig : devConfig)
+const pool = new Pool(NODE_ENV ? proConfig : devConfig)
 
 
 module.exports = {
