1ff69235535f53dcb3b3439dd4ac8b51cacbbd64
Mohamed <mmuseaden@gmail.com>
2021-10-07 20:35:04 +0100

    take20

--- db/index.js
+++ db/index.js
@@ -23,7 +23,7 @@ logger.info('started server in the index.js file off db');
 
 const pool = new Pool(process.env.NODE_ENV ? proConfig : devConfig)
 
-// no pg_hba.conf entry for host "54.237.223.51", user "jxjigseasgqdmy", database "d6u6ojafcgg5l7", SSL off
+
 module.exports = {
   query: (text, params) => {
     return pool.query(text, params)
